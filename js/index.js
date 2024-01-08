72e9901646b8efb5a44e9be19b95d800d03a4ea3
Anthony <skydabutterrs+github@gmail.com>
2024-01-07 20:31:25 -0500

    fix
    loading issue with packaged electron app (worked in testing fine)

--- js/index.js
+++ js/index.js
@@ -1,6 +1,6 @@
 // Calls upon Node.JS filesystem module to read directories synchronously
-const { readdirSync } = require('fs');
-const isTesting = true;
+const { readdirSync, readdir } = require('fs');
+const isTesting = false;
 // Creates an empty audio object array
 const audio = [];
 // Determines the default value of sfx toggle state
@@ -10,17 +10,18 @@ let runningMethods = 1;
 // Determines the default value of sfx cooldown
 // Common test value: 1000
 let maxCooldown = 120000;
+let dir;
 
 if (isTesting) {
-    let dir = './sounds';
+    dir = './sounds';
 } else {
-    let dir = './resources/sounds'
+    dir = '../sounds'
 }
 
 // Reads the /sounds directory asynchrounously (1 file backwards from current directory)
-readdirSync(dir).forEach(file => {
+readdirSync('./resources/app/sounds').forEach(file => {
     // Each directory is it's own mp3 file, and gets added to a list
-    audio.push(new Audio(`${dir}${file}`))
+    audio.push(new Audio(`${dir}/${file}`))
 });
 
 // A function to be called from the button which modifies the sfx toggle state
@@ -48,6 +49,7 @@ const changeMethodCount = () => {
     if (Number.isInteger(obj)) {
         runningMethods = obj;
     } else {
+        document.getElementById('changeSFXCount').value = '1';
         console.log(`Invalid response for SFX count value`)
     }
 }
