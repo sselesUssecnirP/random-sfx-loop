276a10c1f0391aa1e59ab8cb81f0ce6a573bb9df
Anthony <skydabutterrs+github@gmail.com>
2024-01-10 00:09:25 -0500

    convert to typescript
    by convert, I mean just copy code, fix quick errors, and never touch again unless im bored and feeling like changing a random project. Now I can use typescript without setup in the future.

--- js/index.ts
+++ js/index.ts
@@ -1,18 +1,8 @@
-"use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
 // Calls upon Node.JS filesystem module to read directories synchronously
 const { readdirSync } = require('fs');
 const isTesting = false;
 // Creates an empty audio object array
-const audio = [];
+const audio: HTMLAudioElement[] = [];
 // Determines the default value of sfx toggle state
 let sfxEnabled = false;
 // Determines the default value for asynschronous sfx methods
@@ -20,73 +10,80 @@ let runningMethods = 1;
 // Determines the default value of sfx cooldown
 // Common test value: 1000
 let maxCooldown = 120000;
-let dir;
+let dir: string;
+
 if (isTesting) {
     dir = './sounds';
+} else {
+    dir = '../sounds'
 }
-else {
-    dir = '../sounds';
-}
+
 // Reads the /sounds directory asynchrounously (1 file backwards from current directory)
-readdirSync('./resources/app/sounds').forEach((file) => {
+readdirSync('./resources/app/sounds').forEach((file: string) => {
     // Each directory is it's own mp3 file, and gets added to a list
-    audio.push(new Audio(`${dir}/${file}`));
+    audio.push(new Audio(`${dir}/${file}`))
 });
+
 // A function to be called from the button which modifies the sfx toggle state
 const toggleSFX = () => {
     // if sfx is enabled, it disables it
     if (sfxEnabled) {
-        document.getElementById('toggleSFX').innerText = 'Start SFX';
+        document.getElementById('toggleSFX')!.innerText = 'Start SFX';
         sfxEnabled = false;
-        console.log('disabled!');
-        return [`success`, `sfx has been disabled`];
-        // if sfx is disabled, it enables it and runs the requested processes
-    }
-    else {
-        document.getElementById('toggleSFX').innerText = 'Stop SFX';
+        console.log('disabled!')
+        return [`success`, `sfx has been disabled`]
+    // if sfx is disabled, it enables it and runs the requested processes
+    } else {
+        document.getElementById('toggleSFX')!.innerText = 'Stop SFX';
         sfxEnabled = true;
         for (let i = 0; i < runningMethods; i++) {
-            console.log('ran SFX');
-            setTimeout(runSFX, Math.floor(Math.random() * maxCooldown));
+            console.log('ran SFX')
+            setTimeout(runSFX, Math.floor(Math.random() * maxCooldown))
         }
         console.log('enabled!');
-        return [`success`, `sfx has been enabled`];
+        return [`success`, `sfx has been enabled`]
     }
 };
+
 // Modifies the number of sfx processes that run
 const changeMethodCount = () => {
-    let obj = document.getElementById('changeSFXCount');
+    let obj = document.getElementById('changeSFXCount')! as HTMLInputElement;
     let objValue = Number.parseInt(obj.value);
+
     if (Number.isInteger(objValue)) {
         runningMethods = objValue;
         return [`success`, `method count changed to ${objValue}`];
-    }
-    else {
+    } else {
         obj.value = '1';
         console.log(`Invalid response for SFX count value`);
         return [`success`, `method count defaulted to 1`];
     }
 };
+
 // Modifies the number of sfx processes that run
 const changeMethodTime = () => {
-    let obj = document.getElementById('changeSFXCD');
+    let obj = document.getElementById('changeSFXCD') as HTMLInputElement;
     let objValue = Number.parseInt(obj.value);
+
     if (Number.isInteger(objValue)) {
         maxCooldown = objValue * 1000;
         return [`success`, `maxCooldown changed to ${objValue}`];
-    }
-    else {
+    } else {
         obj.value = '120';
         console.log(`Invalid response for SFX cooldown value`);
         return [`fail`, `maxCooldown defaulted to 120 seconds`];
     }
 };
+
 // Runs random sfx per random decimal times maxCooldown
-const runSFX = () => __awaiter(void 0, void 0, void 0, function* () {
-    if (!sfxEnabled)
-        return;
+const runSFX = async () => {
+    if (!sfxEnabled) return;
+
     let num = Math.floor(Math.random() * audio.length);
-    audio[num].play();
-    console.log('played sound!');
-    setTimeout(runSFX, Math.floor(Math.random() * maxCooldown));
-});
+
+    audio[num].play()
+
+    console.log('played sound!')
+
+    setTimeout(runSFX, Math.floor(Math.random() * maxCooldown))
+}
--- start.ts
+++ start.ts
@@ -1,33 +1,39 @@
-"use strict";
 /*
 
     Creates the windows
 
 */
+
 const { app, BrowserWindow, ipcMain } = require('electron');
 const browserPreferences = {
-    initialWindow: {
-        height: 250, width: 250, resizable: false, maximizable: false,
-        webPreferences: {
+    initialWindow: { 
+        height: 250, width: 250, resizable: false, maximizable: false, 
+        webPreferences: { 
             devTools: false, nodeIntegration: true, contextIsolation: false
         }
     }
-};
-function createWindow(file, opts) {
-    const win = new BrowserWindow(opts);
-    win.menuBarVisible = false;
-    win.loadFile(file);
 }
+
+function createWindow(file: string, opts: object) {
+
+    const win = new BrowserWindow(opts);
+
+    win.menuBarVisible = false;
+
+    win.loadFile(file)
+}
+
 app.whenReady().then(() => {
     createWindow("html/index.html", browserPreferences['initialWindow']);
-    console.log('loaded!');
+
+    console.log('loaded!')
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) {
             createWindow("html/index.html", browserPreferences['initialWindow']);
         }
-    });
+      });
 });
+
 app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin')
-        app.quit();
+    if (process.platform !== 'darwin') app.quit()
 });
